52e78e65bee31ee506d29ffff8dea52438930a79
Bx3mE <c.daniel.olsson@gmail.com>
2021-08-22 22:54:02 +0200

    continiue PIO Conversion

---
+++ src/main.h
@@ -0,0 +1,23 @@
+/*
+  main.h - OPAL FW on PJRC Teensy 4.x board
+
+  Part of OpenGalvo - OPAL Firmware
+
+  Copyright (c) 2020-2021 Daniel Olsson
+
+  OPAL Firmware is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPAL Firmware is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPAL Firmware.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+void processMcode(GCode* code);
+void process();
